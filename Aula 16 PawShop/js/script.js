581761027ed68e71840c0f2e661265b7d587155a
Thainara Paulo <thainaradepaulo@gmail.com>
2023-11-14 08:56:56 -0300

    Atividade Banco 90
    Página banco 90

--- Aula 16 PawShop/js/script.js
+++ Aula 16 PawShop/js/script.js
@@ -1,18 +1,25 @@
 // addEventListner =  adicionar um "ouvidor" de eventos 
+const botao = document.querySelector("#botaoTema")
 botao.addEventListener("click",modo)
 
 function modo(){
     // document = html
     // queryselector = selecionar elementos 
-    let body = document.querySelector("body")
-    let botao = document.querySelector("#botaoTema")
-    
+    let body = document.querySelector("body")   
+    let atualTemaClaro = document.querySelector("#temaAtualClaro")   
+    let atualTemaEscuro = document.querySelector("#temaAtualEscuro")   
+
      if(botao.checked){
         // setAtribute (atributo, valor)
         body.setAttribute("data-bs-theme","dark")
+        atualTemaClaro.style.display = "none"
+        atualTemaEscuro.style.display = "block"
 
       }else{
         body.setAttribute("data-bs-theme","light")
+        atualTemaClaro.style.display = "block"
+        atualTemaEscuro.style.display = "none"
+
 
          }
      }
